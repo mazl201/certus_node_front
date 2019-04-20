a01b759842c76747c69ceea4d6b4b2b89cd51fe0
mazl201 <mazl2010@qq.com>
2019-04-20 14:24:31 +0800

    first commit for certus page

--- util/HttpUtils.js
+++ util/HttpUtils.js
@@ -1,6 +1,12 @@
-var server = require("../config/server");
+var path = require('path');
+// 通过NODE_ENV来设置环境变量，如果没有指定则默认为生产环境
+var env = process.env.NODE_ENV || 'production';
+env = env.toLowerCase();
+// 载入配置文件
+var server = path.resolve(__dirname, env).server;
 var http = require("http");
 var request = require("request");
+
 var post = function (url, token, callback) {
     var options = {
         host: server.host,
